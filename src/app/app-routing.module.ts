f3edf466c01baa326efb70ed6a45e7d1d4def4d3
Emily Biegas <emilybiegas@gmail.com>
2020-01-08 14:22:49 -0500

    added basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { RedComponent } from "./red/red.component";
+import { SeventiesComponent } from "./seventies/seventies.component";
+import { HomepageComponent } from "./homepage/homepage.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "homepage", component: HomepageComponent },
+  { path: "seventies", component: SeventiesComponent },
+  { path: "red", component: RedComponent },
+  { path: "", redirectTo: "/homepage", pathMatch: "full" }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SeventiesComponent } from './seventies/seventies.component';
 import { RedComponent } from './red/red.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SeventiesComponent,
-    RedComponent
+    RedComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
